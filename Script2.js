fa28874bb125d54d68f3f9081aec8d1cd889b922
mtswtest <mtswtest@gmail.com>
2020-04-22 12:13:11 -0700

    Create Script2.js

---
+++ Script2.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', event => {
+    let button = document.getElementById('ShowUSBDevices')
+
+    button.addEventListener('click', async () => {
+
+        let device;
+        try {
+            device = await navigator.usb.requestDevice({
+                filters: [{
+                    vendorId: 0x0801
+                }]
+            });
+        } catch (err) {
+            // No device was selected.
+            console.log('No Devcice Found');
+        }
+
+        if (device !== undefined) {
+            // Add |device| to the UI.
+            console.log(device.productName);
+            console.log(device.manufacturerName);
+        }
+        console.log('Done');
+    })
+})
